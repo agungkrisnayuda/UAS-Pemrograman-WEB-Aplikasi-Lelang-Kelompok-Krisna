9cad5c29ee255c0da6520be5741fdbd8fed87692
KrisnaYuda <mangde1205@gmail.com>
2026-06-17 19:05:00 +0800

    Add files via upload

---
+++ Primakara-Auction/Backend/database/migrations/2026_06_12_024744_create_auctions_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('auctions', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('title');
+
+            $table->text('description');
+
+            $table->string('image')->nullable();
+
+            $table->decimal('start_price',12,2);
+
+            $table->decimal('bid_increment',12,2);
+
+            $table->dateTime('starts_at');
+
+            $table->dateTime('ends_at');
+
+            $table->enum('status',[
+                'scheduled',
+                'active',
+                'ended'
+            ])->default('scheduled');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('auctions');
+    }
+};
